a3ed4cd652d03b0aa20d13a97a1c3e32aade44c8
RussianFrost <zak950000@mail.ru>
2024-05-20 16:39:14 +0600

    Translation of a project from js to typescript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './index.css';
+import React from 'react'
 import BottomNavigation from '@mui/material/BottomNavigation';
 import {BottomNavigationAction} from "@mui/material";
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import RouterPanel from './RouterPanel';
 
 function App() {
     return (
     <div className="App">
-        <RouterPanel />
       <BottomNavigation showLabels>
         <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
         <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />

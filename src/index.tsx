080b35fa01b5c0f1353a5410e1e2653a2e8f5b7b
RussianFrost <zak950000@mail.ru>
2024-05-21 21:18:27 +0600

    fix package

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>
+    element: <App />
   }
 ]);
 

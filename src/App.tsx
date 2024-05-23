21be4406634f363f0b233edf8f682fe326174680
RussianFrost <zak950000@mail.ru>
2024-05-23 22:11:53 +0600

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
-import {BottomNavigationAction} from "@mui/material";
+import {BottomNavigationAction, Paper} from "@mui/material";
 import AddchartIcon from '@mui/icons-material/Addchart';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
@@ -19,13 +19,15 @@ function App() {
                 <Outlet />
             </div>
             <div className="App-footer" style={{ width: "100%" }}>
-              <BottomNavigation showLabels value={value} onChange={(event, newValue) => {
-                  setValue(newValue);
-              }}>
-              <Link to={``}> <BottomNavigationAction label="Recents" value="home" icon={<AnalyticsIcon />} /> </Link>
-              <Link to={`createschedule`}> <BottomNavigationAction label="Favorites" value="create" icon={<AddchartIcon />} /> </Link>
-              <Link to={`profile`}> <BottomNavigationAction label="Nearby" value="profile" icon={<AccountBoxIcon />} /> </Link>
-              </BottomNavigation>
+                <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+                  <BottomNavigation showLabels value={value} onChange={(event, newValue) => {
+                      setValue(newValue);
+                  }}>
+                  <Link to={``}> <BottomNavigationAction label="Recents" value="home" icon={<AnalyticsIcon />} /> </Link>
+                  <Link to={`createschedule`}> <BottomNavigationAction label="Favorites" value="create" icon={<AddchartIcon />} /> </Link>
+                  <Link to={`profile`}> <BottomNavigationAction label="Nearby" value="profile" icon={<AccountBoxIcon />} /> </Link>
+                  </BottomNavigation>
+                </Paper>
             </div>
         </div>
   );

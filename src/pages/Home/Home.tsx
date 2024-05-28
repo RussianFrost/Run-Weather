78a109c687f9ff5c3db52a4af40a27aef3fcefc9
RussianFrost <zak950000@mail.ru>
2024-05-28 16:57:29 +0600

    Creating Component Pages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,53 +1,11 @@
 import React from "react";
+import WeatherWelcomeContainer from "./WearthWelcomeContainer";
 
 const Home = () => {
   return (
       <div className="home-page">
-        <div className="welcome-container">
-            <h5>Добро пожаловать</h5>
-            <div className="wearth-block">
-                <p>Лучшее время для бега:</p>
-                <p>15:00 - 17:00</p>
-                <p>24C, 2 m/s</p>
-            </div>
-        </div>
-          <div className="main-home">
-              <p>Погода сегодня</p>
-              <div className="wearth-block">
-                  <div className="wearth-block-indicator">
-                      <div className="wearth-block-indicator-city">
-                          <p>Sonoma</p>
-                          <p>76°</p>
-                      </div>
-                      <div className="wearth-block-indicator-city">
-
-                      </div>
-                  </div>
-                  <div className="wearth-indicator-week">
-
-                  </div>
-              </div>
-              <div className="weather-micro-container">
-                  <div className="weather-micro-block">
-                    <div className="wether-micro-name">
-
-                    </div>
-                  </div>
-                  <div className="weather-micro-block">
-                      <div className="wether-micro-name">
-
-                      </div>
-                  </div>
-                  <div className="weather-micro-block">
-                      <div className="wether-micro-name">
-
-                      </div>
-                  </div>
-                  <div className="weather-micro-block">
-
-                  </div>
-              </div>
-          </div>
+        <WeatherWelcomeContainer />
+        <p>Погода сегодня</p>
       </div>
   )
 };

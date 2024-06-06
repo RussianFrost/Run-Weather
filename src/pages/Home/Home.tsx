0c7b3c1b53955dc41d34ed9dab554a4288f19501
RussianFrost <zak950000@mail.ru>
2024-06-06 17:17:14 +0600

    layout 1/5

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,15 +5,30 @@ import {Paper, Typography} from "@mui/material";
 const Home = () => {
   return (
       <div className="home-page">
-        <Typography variant="h4" gutterBottom>
+        <Typography variant="h5" gutterBottom>
               Добро пожаловать!
         </Typography>
 
-          <Paper className="wearth-block" variant="elevation" elevation={1}>
-              <Typography variant="body1" gutterBottom className="values-wearth-name"> Лучшее время для бега:</Typography>
-              <Typography variant="h5" gutterBottom className="values-wearth"> 15:00 - 17:00 </Typography>
-              <Typography variant="h6" gutterBottom className="values-wearth"> 24 C, 2 m/s </Typography>
+          <Paper className='wearth-block' variant="elevation" elevation={1}>
+              <Typography variant="body1" gutterBottom className='values-wearth-name'> Лучшее время для бега:</Typography>
+              <Typography variant="h5" gutterBottom className='values-wearth'> 15:00 - 17:00 </Typography>
+              <Typography variant="h6" gutterBottom className='values-wearth'> 24 C, 2 m/s </Typography>
+          </Paper>
 
+          <Typography variant="h6" gutterBottom>
+              Погода сегодня:
+          </Typography>
+          <Paper className='wearth-today-block' variant="elevation" elevation={1}>
+                <div className="wearth-city">
+                <Typography variant="button" display="block" gutterBottom> Sonoma </Typography>
+                <Typography variant="h4" display="block" gutterBottom> 76° </Typography>
+                </div>
+                <div className="wearth-status">
+                <Typography variant="button" display="block" gutterBottom> Sunny </Typography>
+                </div>
+                <div className="wearth-week">
+
+                </div>
           </Paper>
           {/*<Card className="wearth-block">*/}
           {/*  */}

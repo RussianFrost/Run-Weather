9227915c0aada803eb193979caf4f85af1132b7d
RussianFrost <zak950000@mail.ru>
2024-06-08 00:25:57 +0600

    layout 1/3

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,6 +5,9 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./Home.css";
 import { Paper, Typography } from "@mui/material";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import CloudIcon from "@mui/icons-material/Cloud";
+import { yellow } from "@mui/material/colors";
 
 const Home = () => {
   return (
@@ -22,20 +25,21 @@ const Home = () => {
         }}
       >
         <Typography variant="body1" className="values-wearth-name">
-          {" "}
           Лучшее время для бега:
         </Typography>
         <Typography variant="h5" className="values-wearth">
-          {" "}
-          15:00 - 17:00{" "}
+          15:00 - 17:00
         </Typography>
         <Typography variant="h6" className="values-wearth">
-          {" "}
-          24 C, 2 m/s{" "}
+          24 C, 2 m/s
         </Typography>
       </Paper>
 
-      <Typography variant="body1" gutterBottom>
+      <Typography
+        variant="subtitle1"
+        className="wearth-typography-today"
+        fontFamily="Roboto"
+      >
         Погода сегодня:
       </Typography>
       <Paper
@@ -47,17 +51,86 @@ const Home = () => {
           color: "white",
         }}
       >
-        <div className="wearth-city">
-          <Typography variant="button"> Sonoma </Typography>
-          <Typography variant="h4"> 76° </Typography>
-        </div>
-        <div className="wearth-status">
-          <Typography variant="button"> Sunny </Typography>
+        <div className="wearth-block-status">
+          <div className="wearth-city">
+            <Typography variant="button"> Sonoma </Typography>
+            <Typography variant="h4"> 76° </Typography>
+          </div>
+          <div className="wearth-status">
+            <WbSunnyIcon fontSize="small" sx={{ color: yellow[600] }} />
+            <Typography variant="button" fontSize="small">
+              {" "}
+              Sunny{" "}
+            </Typography>
+            <Typography variant="button"> H:88° L:57°</Typography>
+          </div>
         </div>
         <div className="wearth-week">
           <div className="wearth-day">
-            <Typography variant="button"> 9AM </Typography>
-              <Typography variant="button"> 9AM </Typography>
+            <Typography variant="button" fontSize="small">
+              {" "}
+              9AM{" "}
+            </Typography>
+            <WbSunnyIcon fontSize="small" sx={{ color: yellow[600] }} />
+            <Typography variant="button" fontSize="small">
+              {" "}
+              77°{" "}
+            </Typography>
+          </div>
+          <div className="wearth-day">
+            <Typography variant="button" fontSize="small">
+              {" "}
+              10AM{" "}
+            </Typography>
+            <WbSunnyIcon fontSize="small" sx={{ color: yellow[600] }} />
+            <Typography variant="button" fontSize="small">
+              {" "}
+              80°{" "}
+            </Typography>
+          </div>
+          <div className="wearth-day">
+            <Typography variant="button" fontSize="small">
+              {" "}
+              11AM{" "}
+            </Typography>
+            <WbSunnyIcon fontSize="small" sx={{ color: yellow[600] }} />
+            <Typography variant="button" fontSize="small">
+              {" "}
+              81°{" "}
+            </Typography>
+          </div>
+          <div className="wearth-day">
+            <Typography variant="button" fontSize="small">
+              {" "}
+              12AM{" "}
+            </Typography>
+            <CloudIcon fontSize="small" />
+            <Typography variant="button" fontSize="small">
+              {" "}
+              81°{" "}
+            </Typography>
+          </div>
+          <div className="wearth-day">
+            <Typography variant="button" fontSize="small">
+              {" "}
+              1PM{" "}
+            </Typography>
+            <CloudIcon fontSize="small" />
+            <Typography variant="button" fontSize="small">
+              {" "}
+              77°{" "}
+            </Typography>
+          </div>
+          <div className="wearth-day">
+            <Typography variant="button" fontSize="small">
+              {" "}
+              2PM{" "}
+            </Typography>
+            <WbSunnyIcon fontSize="small" sx={{ color: yellow[600] }} />
+            <Typography variant="button" fontSize="small">
+              {" "}
+              77°{" "}
+            </Typography>
           </div>
         </div>
       </Paper>

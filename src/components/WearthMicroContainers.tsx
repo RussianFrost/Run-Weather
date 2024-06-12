64a088da5d23785c7a4bffb5f928245e79494a21
RussianFrost <zak950000@mail.ru>
2024-06-13 00:21:11 +0600

    layout 1/2

--- src/components/WearthMicroContainers.tsx
+++ src/components/WearthMicroContainers.tsx
@@ -1,19 +1,33 @@
 import React from "react";
+import {Paper, Typography} from "@mui/material";
+import wearthWeekBlock from "./WearthWeekBlock";
 
-const WeatherMicroContainers = () => {
+type WearherMicroBlocks = {
+    type: any;
+    data: any;
+    time: any;
+    uv: any;
+    weather: any;
+}
+
+const WeatherMicroContainers: React.FC<WearherMicroBlocks> = (wearhermicro) => {
   return (
-    <div className="weather-micro-container">
-      <div className="weather-micro-block">
-        <div className="wether-micro-name"></div>
+      <div className="weather-micro-container">
+          <div className="weather-micro-block">
+            <div className="wether-micro-name">
+                <Typography variant="button" fontSize="small" sx={{color: "grey"}}>
+                    {wearhermicro.type}
+                </Typography>
+                <Typography variant="h4" fontSize="small">
+                    {wearhermicro.data}
+                </Typography>
+                <Typography variant="h6" fontSize="small">
+                    {wearhermicro.uv}
+                    {wearhermicro.weather}
+                </Typography>
+            </div>
+          </div>
       </div>
-      <div className="weather-micro-block">
-        <div className="wether-micro-name"></div>
-      </div>
-      <div className="weather-micro-block">
-        <div className="wether-micro-name"></div>
-      </div>
-      <div className="weather-micro-block"></div>
-    </div>
   );
 };
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,8 +6,9 @@ import "@fontsource/roboto/700.css";
 import "./Home.css";
 import { Paper, Typography } from "@mui/material";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
-import CloudIcon from "@mui/icons-material/Cloud";
 import { yellow } from "@mui/material/colors";
+import WeatherWeekBlock from "../../components/WearthWeekBlock";
+import WeatherMicroContainers from "../../components/WearthMicroContainers";
 
 const Home = () => {
   return (
@@ -59,81 +60,32 @@ const Home = () => {
           <div className="wearth-status">
             <WbSunnyIcon fontSize="small" sx={{ color: yellow[600] }} />
             <Typography variant="button" fontSize="small">
-              {" "}
-              Sunny{" "}
+              Sunny
             </Typography>
             <Typography variant="button"> H:88° L:57°</Typography>
           </div>
         </div>
         <div className="wearth-week">
-          <div className="wearth-day">
-            <Typography variant="button" fontSize="small">
-              {" "}
-              9AM{" "}
-            </Typography>
-            <WbSunnyIcon fontSize="small" sx={{ color: yellow[600] }} />
-            <Typography variant="button" fontSize="small">
-              {" "}
-              77°{" "}
-            </Typography>
-          </div>
-          <div className="wearth-day">
-            <Typography variant="button" fontSize="small">
-              {" "}
-              10AM{" "}
-            </Typography>
-            <WbSunnyIcon fontSize="small" sx={{ color: yellow[600] }} />
-            <Typography variant="button" fontSize="small">
-              {" "}
-              80°{" "}
-            </Typography>
-          </div>
-          <div className="wearth-day">
-            <Typography variant="button" fontSize="small">
-              {" "}
-              11AM{" "}
-            </Typography>
-            <WbSunnyIcon fontSize="small" sx={{ color: yellow[600] }} />
-            <Typography variant="button" fontSize="small">
-              {" "}
-              81°{" "}
-            </Typography>
-          </div>
-          <div className="wearth-day">
-            <Typography variant="button" fontSize="small">
-              {" "}
-              12AM{" "}
-            </Typography>
-            <CloudIcon fontSize="small" />
-            <Typography variant="button" fontSize="small">
-              {" "}
-              81°{" "}
-            </Typography>
-          </div>
-          <div className="wearth-day">
-            <Typography variant="button" fontSize="small">
-              {" "}
-              1PM{" "}
-            </Typography>
-            <CloudIcon fontSize="small" />
-            <Typography variant="button" fontSize="small">
-              {" "}
-              77°{" "}
-            </Typography>
-          </div>
-          <div className="wearth-day">
-            <Typography variant="button" fontSize="small">
-              {" "}
-              2PM{" "}
-            </Typography>
-            <WbSunnyIcon fontSize="small" sx={{ color: yellow[600] }} />
-            <Typography variant="button" fontSize="small">
-              {" "}
-              77°{" "}
-            </Typography>
-          </div>
+          <WeatherWeekBlock time={"9AM"} degrees={"77°"}></WeatherWeekBlock>
+          <WeatherWeekBlock time={"10AM"} degrees={"80°"}></WeatherWeekBlock>
+          <WeatherWeekBlock time={"11AM"} degrees={"81°"}></WeatherWeekBlock>
+          <WeatherWeekBlock time={"12AM"} degrees={"81°"}></WeatherWeekBlock>
+          <WeatherWeekBlock time={"1PM"} degrees={"85°"}></WeatherWeekBlock>
+          <WeatherWeekBlock time={"2PM"} degrees={"88°"}></WeatherWeekBlock>
         </div>
       </Paper>
+        <Paper
+        className="micro-blocks"
+        sx={{
+            backgroundColor: "#5d63d1",
+            color: "white",
+            borderRadius: "20px",
+        }}
+        >
+            <div className="weather-micro-container">
+                <WeatherMicroContainers type = {"UV INDEX"} data={"4"} uv={"Moderate"} time={""} weather={""}></WeatherMicroContainers>
+            </div>
+        </Paper>
     </div>
   );
 };

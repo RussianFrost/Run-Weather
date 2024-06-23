2fc34c7f53067a709e6cf32acf1d0fedf7d5a072
GoodGrim <bayramkulov.86@mail.ru>
2024-06-23 18:16:10 +0300

    Calendar

--- src/pages/CreateSchedule/CreateSchedule.tsx
+++ src/pages/CreateSchedule/CreateSchedule.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import * as React from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import Button from '@mui/material/Button';
 
-const CreateSchedule = () => {
-  return <div>Create Schedule</div>;
-};
-
-export default CreateSchedule;
+export default function BasicDateCalendar() {
+    return (
+        <React.Fragment>
+            <h1 style={{ textAlign: 'center' }}>Создание расписания</h1>
+            <h2 style={{ textAlign: 'center', fontSize: '20px' }}>Выберите даты тренировок:</h2>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateCalendar />
+            </LocalizationProvider>
+            <div style={{ textAlign: 'center' }}>
+                <Button variant="contained" color="primary" style={{borderRadius: '25px', marginLeft: '200px', backgroundColor: '#5D63D1'}}>Далее</Button>
+            </div>
+        </React.Fragment>
+    );
+}

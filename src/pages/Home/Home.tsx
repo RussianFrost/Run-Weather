44f9167a0c387996e17ea4754684732613627bfd
RussianFrost <zak950000@mail.ru>
2024-07-08 17:05:38 +0600

    geolocation 1/2

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,6 +14,7 @@ import {useWeathersApi} from "../../services/useWeathersApi";
 const Home = () => {
 
     const [weatherDay, setWeatherDay] = useState<any>(null);
+    useWeathersApi.geoPosition();
 
     useEffect(() => {
         useWeathersApi.getDayWeather().then((weather) => {
@@ -23,7 +24,7 @@ const Home = () => {
         }).catch(error => {
             console.error(error);
         });
-    }, []);
+    }, []); 
 
   return (
     <div className="home-page">
--- src/services/useWeathersApi.ts
+++ src/services/useWeathersApi.ts
@@ -1,9 +1,31 @@
 import axios from "axios";
 import {BackUrls} from "./backendApi/backUrls";
 import {KeyStorage} from "../utils/keys";
+import { useEffect, useState } from "react";
 
 export class useWeathersApi {
+    static geoPosition(){
+        return useEffect(() => {
+            const [city, setCity] = useState('');
+            if ('geolocation' in navigator) {
+              navigator.geolocation.getCurrentPosition((position) => {
+                fetch(`https://geocode.xyz/${position.coords.latitude},${position.coords.longitude}?json=1`)
+                  .then(response => response.json())
+                  .then(data => {
+                    const city = data.city;
+                    setCity(city);
+                    console.log(city);
+                  })
+                  .catch(error => {
+                    console.error('Ошибка при получении данных', error);
+                  });
+              });
+            } else {
+              console.log('Geolocation не поддерживается вашим браузером');
+            }
+          }, []);
+    }
     static getDayWeather(){
-        return axios.get(`${BackUrls.Prod}/forecast.json?q=Omsk&days=1&key=${KeyStorage.WEATHER_API_KEY}`)
+        return axios.get(`${BackUrls.Prod}/forecast.json?q=${this.geoPosition}&days=1&key=${KeyStorage.WEATHER_API_KEY}`)
     }
 }
